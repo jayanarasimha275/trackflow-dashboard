c33397f2a7c5c821102ed4fb098b8b5c9daa9b2b
jayanarasimha275 <jayanarasimmha232@gmail.com>
2026-07-16 18:55:17 +0530

    fix6

--- app/r/[shortCode]/page.js
+++ app/r/[shortCode]/page.js
@@ -4,7 +4,7 @@ export default async function RedirectPage({ params }) {
   const { shortCode } = await params;
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/r/${shortCode}`,
+    `${process.env.NEXT_PUBLIC_REDIRECT_API_URL}/r/${shortCode}`,
     {
       redirect: "manual",
       cache: "no-store",
