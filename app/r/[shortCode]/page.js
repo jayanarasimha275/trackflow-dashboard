6eb6569c9ca18edf5b811221cdfbeefeb9cdbe6e
jayanarasimha275 <jayanarasimmha232@gmail.com>
2026-07-16 18:41:45 +0530

    fix4

--- app/r/[shortCode]/page.js
+++ app/r/[shortCode]/page.js
@@ -1,26 +1,3 @@
-import { redirect } from "next/navigation";
-
 export default async function RedirectPage({ params }) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/r/${params.shortCode}`,
-    {
-      redirect: "manual",
-      cache: "no-store",
-    },
-  );
-
-  return (
-    <pre>
-      {JSON.stringify(
-        {
-          status: response.status,
-          ok: response.ok,
-          redirected: response.redirected,
-          url: response.url,
-        },
-        null,
-        2,
-      )}
-    </pre>
-  );
+  return <pre>{JSON.stringify(await params, null, 2)}</pre>;
 }
